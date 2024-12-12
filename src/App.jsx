da6cc060359b71181684bfd82e1bff198e5f20fa
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-12-12 14:09:32 +0200

    Adding functionality to paginate brands

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+
+const brandsCollection = [
+  { id: "a1", brand: "Bmw" },
+  { id: "a2", brand: "Mercedes" },
+  { id: "a3", brand: "Audi" },
+  { id: "a4", brand: "VW" },
+  { id: "a5", brand: "Opel" },
+  { id: "a6", brand: "Ford" },
+  { id: "a7", brand: "Porsche" },
+  { id: "a8", brand: "Chevrolet" },
+  { id: "a9", brand: "Dodge" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [brands, setBrands] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(3);
+  const [pages, setPages] = useState(0);
+  const [skip, setSkip] = useState(0);
+
+  useEffect(() => {
+    const sortedBrands = brandsCollection
+      .sort((a, b) => a.brand.localeCompare(b.brand))
+      .slice(skip, skip + limit);
+    setPages(Math.ceil(brandsCollection.length / limit));
+    setBrands(sortedBrands);
+  }, [skip, limit]);
+
+  function prevPageHandler() {
+    setPage((state) => state - 1);
+    setSkip((state) => state - limit);
+  }
+
+  function nextPageHandler() {
+    setPage((state) => state + 1);
+    setSkip((state) => state + limit);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className={styles.main}>
+      <h1>Brands</h1>
+      <div className={styles["items-container"]}>
+        {brands.length !== 0 &&
+          brands.map((e) => (
+            <div className={styles.item} key={e.id}>
+              {e.brand}
+            </div>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className={styles.pagination}>
+        <button onClick={prevPageHandler} disabled={page === 1}>
+          prev
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {page} of {pages}
         </p>
+        <button onClick={nextPageHandler} disabled={page === pages}>
+          next
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
